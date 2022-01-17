0e052df738f1cf4bef84c3d24fff20ce775298e0
Placido81 <markyo81@gmail.com>
2022-01-17 11:42:50 -0500

    first step built server and schema

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+	res.render('server')
+})
+
+app.post('/short', (req, res) => {
+	const db = mongoose.connection.db
+	// insert the record in 'test' collection
+	db.collection('test').insertOne({ testCompleted: 1 })
+
+	res.json({ ok: 1 })
+})
+
+// Setup your mongodb connection here
+mongoose.connect('mongodb://localhost/codedamn', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+})
+mongoose.connection.on('open', () => {
+	// Wait for mongodb connection before server starts
+	app.listen(process.env.PUBLIC_PORT, () => {
+		console.log('Server started')
+	})
+})
